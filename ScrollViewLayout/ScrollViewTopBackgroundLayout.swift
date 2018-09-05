e3d30e7e6d9b0cfa8719bfbffee1462ddb83e662
Dariusz Rybicki <darrarski@gmail.com>
2018-09-05 23:10:16 +0200

    Add ScrollViewTopBackgroundLayout updater

---
+++ ScrollViewLayout/ScrollViewTopBackgroundLayout.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+public class ScrollViewTopBackgroundLayout: ScrollViewLayoutUpdating {
+
+    public init() {}
+
+    // MARK: ScrollViewLayoutUpdating
+
+    public func updateLayout(for view: UIView, in scrollView: UIScrollView) {
+        view.translatesAutoresizingMaskIntoConstraints = true
+        view.frame = CGRect(
+            x: 0,
+            y: scrollView.contentOffset.y,
+            width: scrollView.frame.width,
+            height: max(0, -scrollView.contentOffset.y)
+        )
+    }
+
+}
