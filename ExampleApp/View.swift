fd657e32ddc8e88d8b75e084d713168c95d22eb4
Dariusz Rybicki <darrarski@gmail.com>
2018-09-05 21:08:40 +0200

    Prepare demo app

---
+++ ExampleApp/View.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+class View: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        setupSubviews()
+        setupLayout()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    // MARK: Subviews
+
+    let scrollView = UIScrollView(frame: .zero)
+    let textLabel = UILabel(frame: .zero)
+
+    private func setupSubviews() {
+        scrollView.alwaysBounceVertical = true
+        textLabel.numberOfLines = 0
+        textLabel.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 21))
+
+        addSubview(scrollView)
+        scrollView.addSubview(textLabel)
+    }
+
+    // MARK: Layout
+
+    private func setupLayout() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textLabel.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            textLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+    }
+
+}
--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -2,9 +2,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    // MARK: View
+
     override func loadView() {
-        view = UIView(frame: .zero)
-        view.backgroundColor = .white
+        view = View()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        theView.textLabel.text = .loremIpsum
+    }
+
+    private var theView: View! {
+        return view as? View
     }
 
 }
