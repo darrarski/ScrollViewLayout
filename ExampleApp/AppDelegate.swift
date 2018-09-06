3b7cdd673edff4089373cd149f690cf9f045a58f
Dariusz Rybicki <darrarski@gmail.com>
2018-09-06 02:52:28 +0200

    Setup tab bar items in example app

--- ExampleApp/AppDelegate.swift
+++ ExampleApp/AppDelegate.swift
@@ -9,15 +9,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = tabBarController(with: [
-            Example1ViewController()
+            (Example1ViewController(), "Example 1")
         ])
         window?.makeKeyAndVisible()
         return true
     }
 
-    private func tabBarController(with viewControllers: [UIViewController]) -> UITabBarController {
+    private func tabBarController(with viewControllers: [(UIViewController, String)]) -> UITabBarController {
         let tabBarController = UITabBarController(nibName: nil, bundle: nil)
-        tabBarController.viewControllers = viewControllers.map { UINavigationController(rootViewController: $0) }
+        tabBarController.viewControllers = viewControllers.map { (viewController, title) in
+            let navigationController = UINavigationController(rootViewController: viewController)
+            navigationController.tabBarItem.title = title
+            navigationController.tabBarItem.image = UIColor.black.image(size: CGSize(width: 30, height: 30))
+            return navigationController
+        }
         return tabBarController
     }
 
